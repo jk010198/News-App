b59363346e98d4a86bc0e4ebd58e25275ff89a40
Jayesh <Karalejayesh00@gmail.com>
2023-05-19 11:27:00 +0530

    upload project

--- app/src/main/java/com/infotechworld/newsapp/utill/NetworkResult.kt
+++ app/src/main/java/com/infotechworld/newsapp/utill/NetworkResult.kt
@@ -5,5 +5,4 @@ sealed class NetworkResult<T>(val data: T? = null, val message: String? = null){
     class Sucess<T>(data: T): NetworkResult<T>(data)
     class Error<T>(message: String?, data: T? = null): NetworkResult<T>(data, message)
     class Loading<T>: NetworkResult<T>()
-
 }
