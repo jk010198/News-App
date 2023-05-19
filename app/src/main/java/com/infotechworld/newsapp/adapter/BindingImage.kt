0bb93f5106115c4f1c32c6fdc9d367549cff39fc
Jayesh <Karalejayesh00@gmail.com>
2023-05-19 12:34:01 +0530

    changes in binding adapter

--- app/src/main/java/com/infotechworld/newsapp/adapter/BindingImage.kt
+++ app/src/main/java/com/infotechworld/newsapp/adapter/BindingImage.kt
@@ -1,10 +1,17 @@
 package com.infotechworld.newsapp.adapter
 
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.infotechworld.newsapp.R
 
 @BindingAdapter("bindImg")
 fun ImageView.bindImg(url: String){
-    Glide.with(this.context).load(url).into(this)
+    if(url.equals("null")){
+        Glide.with(this.context).load(R.drawable.newslogo).into(this)
+    } else {
+        Glide.with(this.context).load(url).into(this)
+    }
 }
+//bindImg="@{news.urlToImage != null ? news.urlToImage : `@drawable/newslogo`}"
